50db0abf6cb06212ffcbb558a4892f8e3067e233
reysilvaa <reynaldsilva123@gmail.com>
2026-06-12 05:19:51 +0700

    feat: initialize Turso database client and define schema with seed data

--- src/lib/config/turso.ts
+++ src/lib/config/turso.ts
@@ -36,8 +36,8 @@ export async function setupDatabase() {
     INSERT OR IGNORE INTO users (id, username, password, name) VALUES 
     ('e1b74c2e-4b6c-48c9-8d76-15b561c8f1ea', 'nisa', 'nisa', 'Annisaa Putri Purnomo'),
     ('f2a5b1c3-2d5f-4a8b-9e4c-34f781d9d1bc', 'rey', 'rey', 'Reynald Silva Baktiar'),
-    ('034a2253-5118-4d14-a0a0-dc0a2bc04d2b', 'yus', 'yus', 'Yusriyah Firjatullah');
-    ('034a2253-5118-4d14-a0a0-dc0a2bc04d2b', 'arya', 'arya', ' Arya Bagus');
+    ('034a2253-5118-4d14-a0a0-dc0a2bc04d2b', 'yus', 'yus', 'Yusriyah Firjatullah'),
+    ('b2f153a7-8975-4c00-bc10-18e3a2b5e0c5', 'arya', 'arya', ' Arya Bagus');
     
     UPDATE users SET name = 'Annisaa Putri Purnomo' WHERE username = 'nisa';
     UPDATE users SET name = 'Reynald Silva Baktiar' WHERE username = 'rey';
