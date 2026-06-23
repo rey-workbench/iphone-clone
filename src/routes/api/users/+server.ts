8fa8270ffcd14df7126ecdf1679b28834e164971
reysilvaa <reynaldsilva123@gmail.com>
2026-06-23 14:13:35 +0700

    fix: allow unauthenticated access to user list endpoint by updating apiWrapper configuration

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -6,4 +6,4 @@ const usersService = new UsersService();
 export const GET = apiWrapper(async () => {
 	const users = await usersService.getAllUsers();
 	return { users };
-});
+}, { requireAuth: false });
